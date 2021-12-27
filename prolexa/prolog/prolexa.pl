fd16f8a176c80ec076097f9aad52ec76d394f53f
Phillip Sloan <67047229+phillipSloan@users.noreply.github.com>
2021-12-27 22:34:04 +0000

    Update prolexa.pl

--- prolexa/prolog/prolexa.pl
+++ prolexa/prolog/prolexa.pl
@@ -18,9 +18,9 @@
 :-prompt(_Old,'prolexa> ').
 
 
-%some intial stored rules
-stored_rule(1,[(mortal(X):-human(X))]).
-stored_rule(1,[(human(peter):-true)]).
+% some intial stored rules - commented out for notebook demonstration
+% stored_rule(1,[(mortal(X):-human(X))]).
+% stored_rule(1,[(human(peter):-true)]).
 % stored_rule(1,[(happy(X):-teacher(X))]).
 % stored_rule(1,[(not happy(donald):-true)]).
 
