e88f5a4a1667675b68baa51b04aead3b172cb58d
JE <nl21501@bristol.ac.uk>
2022-01-08 16:34:07 +0000

    remove teacher adj

--- prolexa/prolog/prolexa_grammar.pl
+++ prolexa/prolog/prolexa_grammar.pl
@@ -20,7 +20,7 @@ pred(human,   1,[a/human,n/human]).
 pred(mortal,  1,[a/mortal,n/mortal]).
 % Added for negation
 pred(happy,   1,[a/happy]).
-pred(teacher, 1,[n/teacher, a/teacher]).
+pred(teacher, 1,[n/teacher]).
 pred(student, 1,[n/student]).
 
 
